2e0f97c3226b4fd8394e2da9302b09740514993f
SamTheGamer39 <72585301+SamTheGamer39@users.noreply.github.com>
2022-03-20 11:35:05 -0400

    Made blocks waterloggable.

--- src/main/java/io/github/samthegamer39/railroadblocks/common/blocks/PoleBlock.java
+++ src/main/java/io/github/samthegamer39/railroadblocks/common/blocks/PoleBlock.java
@@ -3,15 +3,28 @@ package io.github.samthegamer39.railroadblocks.common.blocks;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.ShapeContext;
+import net.minecraft.block.Waterloggable;
+import net.minecraft.fluid.FluidState;
+import net.minecraft.fluid.Fluids;
+import net.minecraft.item.ItemPlacementContext;
+import net.minecraft.state.StateManager;
+import net.minecraft.state.property.BooleanProperty;
+import net.minecraft.state.property.Properties;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
 import net.minecraft.util.shape.VoxelShape;
 import net.minecraft.util.shape.VoxelShapes;
 import net.minecraft.world.BlockView;
+import net.minecraft.world.WorldAccess;
 
-public class PoleBlock extends Block {
+public class PoleBlock extends Block implements Waterloggable {
+
+    public static final BooleanProperty WATERLOGGED = Properties.WATERLOGGED;
 
     public PoleBlock(Settings settings) {
         super(settings);
+        setDefaultState(this.getStateManager().getDefaultState()
+                .with(WATERLOGGED, false));
     }
 
     @SuppressWarnings("deprecation") //Why is getOutlineShape()? deprecated? Is there a better alternative?
@@ -20,4 +33,33 @@ public class PoleBlock extends Block {
         return VoxelShapes.cuboid(0.4375, 0.0, 0.4375, 0.5625, 1.0, 0.5625);
     }
 
+    @Override
+    protected void appendProperties(StateManager.Builder<Block, BlockState> stateManager) {
+        stateManager.add(Properties.HORIZONTAL_FACING, WATERLOGGED);
+    }
+
+    @Override
+    public BlockState getPlacementState(ItemPlacementContext context) {
+        return this.getDefaultState()
+                .with(WATERLOGGED, context.getWorld().getFluidState(context.getBlockPos()).getFluid() == Fluids.WATER);
+    }
+
+    @SuppressWarnings("deprecation") //Why is getFluidState() deprecated? Is there a better alternative?
+    @Override
+    public FluidState getFluidState(BlockState state) {
+        return state.get(WATERLOGGED) ? Fluids.WATER.getStill(false) : super.getFluidState(state);
+    }
+
+    @SuppressWarnings("deprecation") //Why is getStateForNeighborUpdate deprecated? Is there a better alternative?
+    @Override
+    public BlockState getStateForNeighborUpdate(BlockState state, Direction direction, BlockState neighborState, WorldAccess world, BlockPos position, BlockPos neighborPos) {
+        if (state.get(WATERLOGGED)) {
+            //1.18+ Versions
+            world.createAndScheduleFluidTick(position, Fluids.WATER, Fluids.WATER.getTickRate(world));
+            //1.17 and Older Versions
+            //world.getFluidTickScheduler().schedule(position, Fluids.WATER, Fluids.WATER.getTickRate(world));
+        }
+
+        return super.getStateForNeighborUpdate(state, direction, neighborState, world, position, neighborPos);
+    }
 }
--- src/main/java/io/github/samthegamer39/railroadblocks/common/blocks/SignBlock.java
+++ src/main/java/io/github/samthegamer39/railroadblocks/common/blocks/SignBlock.java
@@ -1,28 +1,33 @@
 package io.github.samthegamer39.railroadblocks.common.blocks;
 
-import net.minecraft.block.Block;
-import net.minecraft.block.BlockState;
-import net.minecraft.block.HorizontalFacingBlock;
-import net.minecraft.block.ShapeContext;
+import net.minecraft.block.*;
+import net.minecraft.fluid.FluidState;
+import net.minecraft.fluid.Fluids;
 import net.minecraft.item.ItemPlacementContext;
 import net.minecraft.state.StateManager;
+import net.minecraft.state.property.BooleanProperty;
 import net.minecraft.state.property.Properties;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
 import net.minecraft.util.shape.VoxelShape;
 import net.minecraft.util.shape.VoxelShapes;
 import net.minecraft.world.BlockView;
+import net.minecraft.world.WorldAccess;
 
-public class SignBlock extends HorizontalFacingBlock {
+public class SignBlock extends HorizontalFacingBlock implements Waterloggable {
+
+    public static final BooleanProperty WATERLOGGED = Properties.WATERLOGGED;
 
     public SignBlock(Settings settings) {
         super(settings);
-        setDefaultState(this.stateManager.getDefaultState().with(Properties.HORIZONTAL_FACING, Direction.NORTH));
+        setDefaultState(this.stateManager.getDefaultState()
+                .with(Properties.HORIZONTAL_FACING, Direction.NORTH)
+                .with(WATERLOGGED, false)); //Sets default waterlogged state to false.
     }
 
     @Override
     protected void appendProperties(StateManager.Builder<Block, BlockState> stateManager) {
-        stateManager.add(Properties.HORIZONTAL_FACING);
+        stateManager.add(Properties.HORIZONTAL_FACING, WATERLOGGED);
     }
 
     @SuppressWarnings("deprecation") //Why is getOutlineShape()? deprecated? Is there a better alternative?
@@ -40,7 +45,27 @@ public class SignBlock extends HorizontalFacingBlock {
 
     @Override
     public BlockState getPlacementState(ItemPlacementContext context) {
-        return this.getDefaultState().with(Properties.HORIZONTAL_FACING, context.getPlayerFacing().getOpposite());
+        return this.getDefaultState()
+                .with(Properties.HORIZONTAL_FACING, context.getPlayerFacing().getOpposite())
+                .with(WATERLOGGED, context.getWorld().getFluidState(context.getBlockPos()).getFluid() == Fluids.WATER); //Sets waterlogged state based on the presence of water.
     }
 
+    @SuppressWarnings("deprecation") //Why is getFluidState() deprecated? Is there a better alternative?
+    @Override
+    public FluidState getFluidState(BlockState state) {
+        return state.get(WATERLOGGED) ? Fluids.WATER.getStill(false) : super.getFluidState(state); //Displays water when waterlogged.
+    }
+
+    @SuppressWarnings("deprecation") //Why is getStateForNeighborUpdate deprecated? Is there a better alternative?
+    @Override
+    public BlockState getStateForNeighborUpdate(BlockState state, Direction direction, BlockState neighborState, WorldAccess world, BlockPos position, BlockPos neighborPos){
+        if (state.get(WATERLOGGED)) {
+            //1.18+ Versions
+            world.createAndScheduleFluidTick(position, Fluids.WATER, Fluids.WATER.getTickRate(world));
+            //1.17 and Older Versions
+            //world.getFluidTickScheduler().schedule(position, Fluids.WATER, Fluids.WATER.getTickRate(world));
+        }
+
+        return super.getStateForNeighborUpdate(state, direction, neighborState, world, position, neighborPos);
+    }
 }
