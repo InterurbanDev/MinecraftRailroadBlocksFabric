b569e79c10f35059a79f4ca6cce7add8ef6260ff
SamTheGamer39 <72585301+samthegamer39@users.noreply.github.com>
2022-04-15 16:31:06 -0400

    Refactored some code.

--- src/main/java/io/github/samthegamer39/railroadblocks/init/BlockInit.java
+++ src/main/java/io/github/samthegamer39/railroadblocks/init/BlockInit.java
@@ -5,20 +5,18 @@ import io.github.samthegamer39.railroadblocks.common.blocks.SignBlock;
 import io.github.samthegamer39.railroadblocks.common.blocks.SmallSignBlock;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.minecraft.block.Material;
-import net.minecraft.item.AxeItem;
-import net.minecraft.item.ToolItem;
 import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
 public class BlockInit {
 
-    public static final SignBlock WOODEN_CROSSBUCK_BLOCK = new SignBlock(FabricBlockSettings.of(Material.WOOD)
-            .strength(1)
-            .sounds(BlockSoundGroup.WOOD)
+    public static final SignBlock IRON_CROSSBUCK_BLOCK = new SignBlock(FabricBlockSettings.of(Material.METAL)
+            .strength(2)
+            .sounds(BlockSoundGroup.METAL)
     );
 
-    public static final SignBlock IRON_CROSSBUCK_BLOCK = new SignBlock(FabricBlockSettings.of(Material.METAL)
+    public static final PoleBlock IRON_POLE_BLOCK = new PoleBlock(FabricBlockSettings.of(Material.METAL)
             .strength(2)
             .sounds(BlockSoundGroup.METAL)
     );
@@ -28,28 +26,28 @@ public class BlockInit {
             .sounds(BlockSoundGroup.METAL)
     );
 
-    public static final PoleBlock WOODEN_POLE_BLOCK = new PoleBlock(FabricBlockSettings.of(Material.WOOD)
-            .strength(1)
-            .sounds(BlockSoundGroup.WOOD)
-    );
-
-    public static final PoleBlock IRON_POLE_BLOCK = new PoleBlock(FabricBlockSettings.of(Material.METAL)
-            .strength(2)
-            .sounds(BlockSoundGroup.METAL)
-    );
-
     public static final SmallSignBlock WHISTLE_SIGN_BLOCK = new SmallSignBlock(FabricBlockSettings.of(Material.METAL)
             .strength(2)
             .sounds(BlockSoundGroup.METAL)
     );
 
+    public static final SignBlock WOODEN_CROSSBUCK_BLOCK = new SignBlock(FabricBlockSettings.of(Material.WOOD)
+            .strength(1)
+            .sounds(BlockSoundGroup.WOOD)
+    );
+
+    public static final PoleBlock WOODEN_POLE_BLOCK = new PoleBlock(FabricBlockSettings.of(Material.WOOD)
+            .strength(1)
+            .sounds(BlockSoundGroup.WOOD)
+    );
+
     public static void RegisterBlocks() {
-        Registry.register(Registry.BLOCK, new Identifier("railroadblocksaddon", "wooden_crossbuck"), WOODEN_CROSSBUCK_BLOCK);
         Registry.register(Registry.BLOCK, new Identifier("railroadblocksaddon", "iron_crossbuck"), IRON_CROSSBUCK_BLOCK);
-        Registry.register(Registry.BLOCK, new Identifier("railroadblocksaddon", "rxr_advance_sign"), RXR_ADVANCE_SIGN_BLOCK);
-        Registry.register(Registry.BLOCK, new Identifier("railroadblocksaddon", "wooden_pole"), WOODEN_POLE_BLOCK);
         Registry.register(Registry.BLOCK, new Identifier("railroadblocksaddon", "iron_pole"), IRON_POLE_BLOCK);
+        Registry.register(Registry.BLOCK, new Identifier("railroadblocksaddon", "rxr_advance_sign"), RXR_ADVANCE_SIGN_BLOCK);
         Registry.register(Registry.BLOCK, new Identifier("railroadblocksaddon", "whistle_sign"), WHISTLE_SIGN_BLOCK);
+        Registry.register(Registry.BLOCK, new Identifier("railroadblocksaddon", "wooden_crossbuck"), WOODEN_CROSSBUCK_BLOCK);
+        Registry.register(Registry.BLOCK, new Identifier("railroadblocksaddon", "wooden_pole"), WOODEN_POLE_BLOCK);
     }
 
 }
--- src/main/java/io/github/samthegamer39/railroadblocks/init/ItemInit.java
+++ src/main/java/io/github/samthegamer39/railroadblocks/init/ItemInit.java
@@ -12,20 +12,12 @@ import static io.github.samthegamer39.railroadblocks.init.BlockInit.*;
 
 public class ItemInit {
 
-    public static final Item WOODEN_POLE_ITEM =
-            new BlockItem(WOODEN_POLE_BLOCK, new FabricItemSettings()
+    public static final Item IRON_CROSSBUCK_ITEM =
+            new BlockItem(IRON_CROSSBUCK_BLOCK, new FabricItemSettings()
                     .group(RailroadBlocks.RAILROAD_BLOCKS));
 
     public static final Item IRON_POLE_ITEM =
             new BlockItem(IRON_POLE_BLOCK, new FabricItemSettings()
-                    .group(RailroadBlocks.RAILROAD_BLOCKS));
-
-    public static final Item WOODEN_CROSSBUCK_ITEM =
-            new BlockItem(WOODEN_CROSSBUCK_BLOCK, new FabricItemSettings()
-                    .group(RailroadBlocks.RAILROAD_BLOCKS));
-
-    public static final Item IRON_CROSSBUCK_ITEM =
-            new BlockItem(IRON_CROSSBUCK_BLOCK, new FabricItemSettings()
                     .group(RailroadBlocks.RAILROAD_BLOCKS));
 
     public static final Item RXR_ADVANCE_SIGN_ITEM =
@@ -36,13 +28,21 @@ public class ItemInit {
             new BlockItem(WHISTLE_SIGN_BLOCK, new FabricItemSettings()
                     .group(RailroadBlocks.RAILROAD_BLOCKS));
 
+    public static final Item WOODEN_CROSSBUCK_ITEM =
+            new BlockItem(WOODEN_CROSSBUCK_BLOCK, new FabricItemSettings()
+                    .group(RailroadBlocks.RAILROAD_BLOCKS));
+
+    public static final Item WOODEN_POLE_ITEM =
+            new BlockItem(WOODEN_POLE_BLOCK, new FabricItemSettings()
+                    .group(RailroadBlocks.RAILROAD_BLOCKS));
+
     public static void RegisterItems() {
-        Registry.register(Registry.ITEM, new Identifier("railroadblocksaddon", "wooden_crossbuck"), WOODEN_CROSSBUCK_ITEM);
         Registry.register(Registry.ITEM, new Identifier("railroadblocksaddon", "iron_crossbuck"), IRON_CROSSBUCK_ITEM);
-        Registry.register(Registry.ITEM, new Identifier("railroadblocksaddon", "rxr_advance_sign"), RXR_ADVANCE_SIGN_ITEM);
-        Registry.register(Registry.ITEM, new Identifier("railroadblocksaddon", "wooden_pole"), WOODEN_POLE_ITEM);
         Registry.register(Registry.ITEM, new Identifier("railroadblocksaddon", "iron_pole"), IRON_POLE_ITEM);
+        Registry.register(Registry.ITEM, new Identifier("railroadblocksaddon", "rxr_advance_sign"), RXR_ADVANCE_SIGN_ITEM);
         Registry.register(Registry.ITEM, new Identifier("railroadblocksaddon", "whistle_sign"), WHISTLE_SIGN_ITEM);
+        Registry.register(Registry.ITEM, new Identifier("railroadblocksaddon", "wooden_crossbuck"), WOODEN_CROSSBUCK_ITEM);
+        Registry.register(Registry.ITEM, new Identifier("railroadblocksaddon", "wooden_pole"), WOODEN_POLE_ITEM);
     }
 
 }
